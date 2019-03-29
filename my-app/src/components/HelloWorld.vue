25247a200dcdb7a9ad6297fa8f2bde3161824057
Yuhei Jotaki <jotaki@cloudpack.jp>
2019-03-29 09:56:48 +0900

    update: App.vue

--- my-app/src/components/HelloWorld.vue
+++ my-app/src/components/HelloWorld.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="hello">
-    <p>{{ msg }}</p>
+  <div>
+    <table border="1">
+      <tr>
+        <th>商品コード</th>
+        <th>商品名</th>
+      </tr>
+      <tr v-for="item in products" v-bind:key="item.code">
+        <td>{{item.code}}</td>
+        <td>{{item.name}}</td>
+      </tr>
+    </table>
+    <p>ウィンドウの横幅：{{width}}</p>
+    <p>ウィンドウの高さ：{{height}}</p>
   </div>
 </template>
 
@@ -9,9 +20,40 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Hello World.'
+      products: [
+        {
+          code: 'A01',
+          name: 'プロダクトA',
+        },
+        {
+          code: 'B01',
+          name: 'プロダクトB',
+        },
+        {
+          code: 'C01',
+          name: 'プロダクトC',
+        }
+      ],
+      width: window.innerWidth,
+      height: window.innerHeight
     }
-  }
+  },
+  created: function() {
+    // イベントハンドラを登録
+    addEventListener('resize', this.resizeHandler);
+  },
+  beforeDestroy: function() {
+    // イベントハンドラを削除
+    removeEventListener('resize', this.resizeHandler);
+  },
+  methods: {
+    // イベントハンドラ
+    resizeHandler: function($event) {
+      // 現在のウィンドウサイズでプロパティを更新
+      this.width = $event.target.innerWidth;
+      this.height = $event.target.innerHeight;
+    }
+  },
 }
 </script>
 
